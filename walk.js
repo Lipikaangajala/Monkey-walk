90801f02ba6d7954066d067e0b08cf772debbae8
Lipikaangajala <93141716+Lipikaangajala@users.noreply.github.com>
2021-11-01 20:05:34 +0530

    Add files via upload

---
+++ walk.js
@@ -0,0 +1,9 @@
+var name=["Monkey_01(2).png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png"];
+var i=0;
+function walkingmonkey(){
+    if(i==9){
+        i=0;
+    }
+    document.getElementById("monk").src=name[i];
+    i++;
+}
